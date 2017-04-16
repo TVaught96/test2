547313ac6b17b5cfa11caeeab4e54457d6bdb0bc
Timothy Vaught <tvaught@email.uscupstate.edu>
2017-04-16 05:35:54 +0000

    Finished?

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,6 +12,10 @@ class StaticPagesController < ApplicationController
   end
 
   def customer_reviews
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def contact_us
